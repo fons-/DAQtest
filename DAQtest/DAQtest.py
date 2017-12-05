6a47421e8c62117b6535d6896636671a2276d40a
Fons van der Plas <fonsvdplas@gmail.com>
2017-12-05 13:06:06 +0100

    Reading and writing voltage

--- DAQtest/DAQtest.py
+++ DAQtest/DAQtest.py
@@ -1,1 +1,34 @@
 
+from PyDAQmx import Task
+import PyDAQmx
+
+import numpy as np
+import time
+
+from MultiChannelAnalogInput import *
+
+
+writeTask = Task()
+writeTask.CreateAOVoltageChan("/Dev1/ao0","",0.0,5.0,PyDAQmx.DAQmx_Val_Volts,None)
+
+anIn = MultiChannelAnalogInput(["Dev1/ai0"])
+anIn.configure()
+
+
+def setVoltage(value):
+    writeTask.StartTask()
+    writeTask.WriteAnalogScalarF64(1,5.0,1.5927*value,None)
+    writeTask.StopTask()
+
+
+def getVoltage(freq=10000, Nsamples=50):
+    sum = 0;
+    for i in range(Nsamples):
+        sum += anIn.read()
+        time.sleep(1/freq)
+    return sum/Nsamples
+
+
+while True:
+    print(getVoltage())
+    time.sleep(.5)
